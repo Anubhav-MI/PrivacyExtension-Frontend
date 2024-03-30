3f7fa05b9de81a7d242e74e2d156b06c9ede5aaf
Anubhav-MI <120900218+Anubhav-MI@users.noreply.github.com>
2024-03-30 19:46:18 +0530

    frontend updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import axios from "axios";
+import "./app.css";
 function App() {
   const fetchandSetData = async () => {
-    const response = await axios.post("http://localhost:5000/scrapper");
-    console.log(response);
+    try {
+      const response = await axios.post("http://localhost:5000/scrapper", {
+        url: "https://www.jpmorganchase.com/about/terms-and-conditions#:~:text=If%20you%20choose%20to%20link,Chase%20warrant%20that%20such%20site",
+        // You can include any other relevant data here
+      });
+      console.log(response.data); // Handle the response data as needed
+    } catch (error) {
+      console.error("Error fetching terms and conditions:", error);
+    }
   };
+  fetchandSetData();
   return (
     <div className="App">
       <p>Extension</p>
