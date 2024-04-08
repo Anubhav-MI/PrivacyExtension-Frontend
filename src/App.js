6218ed26a5e8fbe7ab0174c2f2890da43b4ba8f7
Anubhav-MI <120900218+Anubhav-MI@users.noreply.github.com>
2024-04-08 19:33:41 +0530

    model integrated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import axios from "axios";
 import "./app.css";
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const [text, setText] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const fetchAndSendData = async (text) => {
     try {
+      setLoading(true);
       const response = await axios.post("http://localhost:5000/summarize", {
         text,
       });
       console.log(response.data);
+      setLoading(false);
       setText(response.data);
     } catch (error) {
       console.error("Error sending text to the backend:", error);
@@ -31,9 +36,28 @@ function App() {
 
   return (
     <div className="App">
-      <p>Extension</p>
-      <p>Terms and Conditions:</p>
-      <p className="text-area">{text}</p>
+      <p className="head">TERMS AND CONDITIONS</p>
+      <div className="text-area">
+        {
+          loading && (
+            <div className="loader">
+              <FontAwesomeIcon icon={faSpinner} spin />
+            </div>
+          ) /* Loading spinner */
+        }
+        {!loading &&
+          text
+            .split(".")
+            .slice(0, -1)
+            .map((line, index) => (
+              <ul key={index}>
+                <li>
+                  <span>{line.trim()}</span>
+                  <br />
+                </li>
+              </ul>
+            ))}
+      </div>
     </div>
   );
 }
